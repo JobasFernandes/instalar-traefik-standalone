2a27b4fb4c0672acbb829ac808be403f5e49ef71
JSFernandes <joseph.automatus@gmail.com>
2024-03-08 09:24:43 -0300

    Ajuste para instalar em x86_64 e aarch64

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+WHITE='\033[1;37m'
+RED='\033[1;31m'
+YELLOW="\033[1;33m"
 
-printf "${WHITE} >> Instalando Docker...\n"
-ip_atual=$(curl -s http://checkip.amazonaws.com)
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker $(whoami)
-docker network create proxy
+ARCH=$(uname -m)
+
+if [ "$ARCH" == "x86_64" ]; then
+    printf " >> ${GREEN}Instalando Docker para arquitetura x86_64...${WHITE} \n"
+    sleep 3
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce
+    sudo usermod -aG docker $(whoami)
+    docker network create proxy
+elif [ "$ARCH" == "aarch64" ]; then
+    printf " >> ${GREEN}Instalando Docker para arquitetura aarch64...${WHITE} \n"
+    sleep 3
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce
+    sudo usermod -aG docker $(whoami)
+    docker network create proxy
+else
+    printf "${RED}Arquitetura não suportada: $ARCH\n"
+    exit 1
+fi
