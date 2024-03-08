82cdd1b0424a5e22664a60ce1eb98df70e89a05b
Joseph <contato@pnztech.com.br>
2024-03-08 17:03:32 -0300

    Ajuste no install do docker - Atualiza a VPS - Configura Timezone

--- install.sh
+++ install.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 GREEN='\033[1;32m'
-BLUE='\033[1;34m'
 WHITE='\033[1;37m'
-RED='\033[1;31m'
-YELLOW="\033[1;33m"
+
+printf " >> ${GREEN}Atualizando a VPS...${WHITE} \n"
+sleep 3
+UBUNTU_VERSION=$(lsb_release -sr)
+if [ "$UBUNTU_VERSION" == "22.04" ]; then
+    echo " > Versao do Ubuntu é $UBUNTU_VERSION. Proseguindo com a atualização..."
+    echo
+    sleep 2
+
+    if grep -q "NEEDRESTART_MODE" /etc/needrestart/needrestart.conf; then
+        sudo sed -i 's/^NEEDRESTART_MODE=.*/NEEDRESTART_MODE=a/' /etc/needrestart/needrestart.conf
+    else
+        echo 'NEEDRESTART_MODE=a' | sudo tee -a /etc/needrestart/needrestart.conf
+    fi
+    echo
+    sleep 2
+else
+    echo " > Versao do Ubuntu é $UBUNTU_VERSION. Proseguindo com a atualização..."
+    echo
+    sleep 2
+fi
+sudo apt-mark hold linux-firmware >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential -y >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apparmor-utils >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y zip >/dev/null 2>&1
+
+printf " >> ${GREEN}Configurando timezone...${WHITE} \n"
+sleep 3
+timedatectl set-timezone America/Sao_Paulo
 
 ARCH=$(uname -m)
 
